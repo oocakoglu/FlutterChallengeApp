ce4f18f3403af4310c125fcca8ab651b234764e4
oocakoglu <ocakoglu@live.com>
2022-01-04 10:34:26 +0300

    Widget Test

--- test/get_odatafn_test.dart
+++ test/get_odatafn_test.dart
@@ -3,7 +3,7 @@ import 'package:test/apis/apiservices.dart';
 import 'package:test/model/filtermodel.dart';
 
 void main() {
-  test("getODATA function test", () async {
+  test("getODATA function test Country", () async {
     //ARRANGE
     FilterModel filterModel = FilterModel(countryCode: "U.S.");
 
@@ -13,5 +13,24 @@ void main() {
 
     //ASSERTION
     expect(oDataFilter != "", true);
+    expect(
+        oDataFilter == "?\$filter=true  and address1_country eq 'U.S.'", true);
+  });
+
+  test("getODATA function test General", () async {
+    //ARRANGE
+    FilterModel filterModel =
+        FilterModel(stateCode: "WA", name: "City", accountnumber: "ABC");
+
+    //ACT,
+    APIServices _api = APIServices();
+    String oDataFilter = _api.getodataFilter(filterModel);
+
+    //ASSERTION
+    expect(oDataFilter != "", true);
+    expect(
+        oDataFilter ==
+            "?\$filter=true  and address1_stateorprovince eq 'WA' and contains(name, 'City') and contains(accountnumber, 'ABC')",
+        true);
   });
 }
